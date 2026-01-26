a8b6fa5e59692f528c2ed42d2429bed40ddf841b
ADMIN <duongduy7070@gmail.com>
2026-01-27 01:33:22 +0700

    session 28

---
+++ BaiTapBuoi28/ex01.ts
@@ -0,0 +1,23 @@
+interface PartTime {
+    id: number,
+    name: string,
+    salary: number,
+    hoursWorked: number
+}
+
+const partTime: PartTime = {
+    id: 2,
+    name: "Binh",
+    salary: 20,
+    hoursWorked: 40
+}
+
+const updatedPartTime: PartTime = {
+    id: 2,
+    name: "Binh",
+    salary: 20,
+    hoursWorked: 45
+}
+
+
+
---
+++ BaiTapBuoi28/ex02.ts
@@ -0,0 +1,44 @@
+interface Employee {
+    id: number
+    name: string
+    salary: number
+    getSalary(): number
+}
+
+class FullTimeEmployee implements Employee {
+    id: number
+    name: string
+    salary: number
+    getSalary(): number {
+        return this.salary;
+    }
+}
+
+class PartTimeEmployee implements Employee {
+    id: number
+    name: string
+    salary: number
+    hoursWorked: number
+    getSalary(): number {
+        return this.salary * this.hoursWorked;
+    }
+}
+
+function calculateTotalSalary(employee: Employee): number {
+    return employee.getSalary();
+}
+
+const fullTimeEmployee = new FullTimeEmployee();
+fullTimeEmployee.id = 1;
+fullTimeEmployee.name = "Duong";
+fullTimeEmployee.salary = 50;
+const partTimeEmployee = new PartTimeEmployee();
+partTimeEmployee.id = 2;
+partTimeEmployee.name = "Duy";
+partTimeEmployee.salary = 14;
+partTimeEmployee.hoursWorked = 2;
+
+console.log(calculateTotalSalary(fullTimeEmployee));
+console.log(calculateTotalSalary(partTimeEmployee));
+
+
